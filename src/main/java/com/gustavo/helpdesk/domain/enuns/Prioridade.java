a2b66dfe78786807893242feaaf1187bcdb90742
GustavoRodrigue <gustavorodriguesgra13@gmail.com>
2022-07-05 22:06:22 -0300

    repositores criados

--- src/main/java/com/gustavo/helpdesk/domain/enuns/Prioridade.java
+++ src/main/java/com/gustavo/helpdesk/domain/enuns/Prioridade.java
@@ -1,7 +1,8 @@
 package com.gustavo.helpdesk.domain.enuns;
 
 public enum Prioridade {
-	ABERTO(0, "ABERTO"), ANDAMENTO(1, "ANDAMENTO"), ENCERRADO(2, "ENCERRADO");
+	BAIXA(0, "BAIXA"), MEDIA(1, "MEDIA"), ALTA(2, "ALTA");
+	
 
 	private Integer codigo;
 	private String descricao;
---
+++ src/main/java/com/gustavo/helpdesk/repositories/PessoaRepository.java
@@ -0,0 +1,9 @@
+package com.gustavo.helpdesk.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gustavo.helpdesk.domain.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+}
